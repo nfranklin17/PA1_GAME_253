c9a75c7208b0341dfd22220479cb0b4c354e0ac1
Nick Gutierrez <cimbyote@gmail.com>
2024-09-08 23:22:07 -0500

    Finished PA1

---
+++ PA1/PA1_4.cs
@@ -0,0 +1,45 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    DIRECTIONS:
+    Modify the "doStuff" function.
+    
+    Declare at least three integer variables and assign initial values to them.
+    
+    Perform the following calculations using the operators listed above, and display the results to the console.
+    
+    For each calculation, print a message describing the operation being performed and the result.
+
+    1) Add two of the numbers together and display the result.
+    2) Subtract one number from another and display the result.
+    3) Multiply two numbers and display the result.
+    4) Divide one number by another and display the result.
+    5) Find the remainder when one number is divided by another and display the result.
+    6) Create a small calculation where you combine at least three operators in a single expression.
+
+    I provide you one variable and one output statement as an example.
+    Notice that when you add a string and a number it will put them together as a string?
+
+    NOTE: You should not "hard code" the output. 
+
+    The output should look like this: 
+        The sum of 10 and 5 is 15
+        The result of subtracting 5 from 10 is 5
+        The result of multiplying 10 by 3 is 30
+        The result of dividing 10 by 5 is 2
+        The remainder when dividing 10 by 3 is 1
+        The result of (a + b) * c % b is 0
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+using System;
+
+namespace PA1_4
+{
+    public class Problem
+    {
+        public void doStuff( )
+        {
+            int a = 10; //example variable
+            Console.WriteLine("The value of a is " + a); // example output statement
+        }
+    }
+}
